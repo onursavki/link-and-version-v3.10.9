0bc35496b7984bc82db960dde4e8462aff057dfe
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-05 14:42:07 +0300

    link-and-version.js

--- link-and-version.js
+++ link-and-version.js
@@ -1,192 +1,57 @@
 // MOD LİNKLERİ
 const links = [
-  	// Otobüsler
-    &quot;&quot;, // Neoplan Cityliner
-    &quot;&quot;, // Neoplan Starliner
-    &quot;&quot;, // Neoplan Tourliner C13
-    &quot;x&quot;, // Neoplan Tourliner C14
-    &quot;&quot;, // Man Lions R07
-    &quot;&quot;, // Man Lions 2023
-    &quot;x&quot;, // Mercedes-Benz O-403
-    &quot;&quot;, // Yeni Tourismo 16RHD
-    &quot;&quot;, // Tourismo 16RHD E5 ve E6
-    &quot;x&quot;, // Tourismo 17RHD
-    &quot;&quot;, // Travego SE 15SHD
-    &quot;&quot;, // Travego SE 17SHD
-    &quot;&quot;, // Yeni Travego 15SHD
-    &quot;&quot;, // Yeni Travego 16SHD
-    &quot;&quot;, // 2023 Travego 16SHD
-    &quot;&quot;, // Setra S516HD
-    &quot;&quot;, // Setra 517HDH
-    &quot;https://ay.live/S531DT&quot;, // Setra S531DT
-
-    // Haritalar
-    &quot;&quot;, // Paris Map 1:1
-    &quot;&quot;, // Roextended Türkiye Haritası
-    &quot;&quot;, // Roextended Türkiye Haritası
-    &quot;&quot;, // Roextended Türkiye Haritası
-    &quot;&quot;, // Roextended Türkiye Haritası
-    &quot;&quot;, // Grand Utopia 1:1
-    &quot;https://ay.live/TasMap&quot;, // Taşıyıcı Map
-    &quot;&quot;, // Türkiye YKS Haritası
-    &quot;&quot;, // Türkiye YKS Haritası
-    &quot;&quot;, // Türkiye YKS Haritası
-
-    // Sayfalar
-    &quot;&quot;, // Başvuru Formu
-    &quot;&quot;, // Reklam ve İş Birliği  
-  
-    // Son eklenen otobüsler  
-    &quot;&quot;, // Temsa Maraton 3 VIP 2019-2023
-    &quot;&quot;, // Temsa Safir Plus HD 13
-    &quot;&quot;, // Temsa Safir 2 VIP
-    &quot;&quot;, // Yeni Tourismo 16RHD 2024
-    &quot;&quot;, // 2023 Travego 16SHD
+    "", // Neoplan Cityliner
+    "", // Neoplan Starliner
+    "", // Neoplan Tourliner C13
+    "x", // Neoplan Tourliner C14
+    "", // Man Lions R07
+    "", // Man Lions 2023
+    "x", // Mercedes-Benz O-403
+    "", // Yeni Tourismo 16RHD
+    "", // Tourismo 16RHD E5 ve E6
+    "x", // Tourismo 17RHD
+    "", // Travego SE 15SHD
+    "", // Travego SE 17SHD
+    "", // Yeni Travego 15SHD
+    "", // Yeni Travego 16SHD
+    "", // 2023 Travego 16SHD
+    "", // Setra S516HD
+    "", // Setra 517HDH
+    "https://ay.live/S531DT", // Setra S531DT
+    "", "", "", "", "", "", "https://ay.live/TasMap", "", "", "",
+    "", "", "", "", "",
+    "", "", "", "", ""
 ];
 
 // SKİN LİNKLERİ
 const skinLinks = [
-  	// Otobüsler
-    &quot;&quot;, // Neoplan Cityliner
-    &quot;&quot;, // Neoplan Starliner
-    &quot;&quot;, // Neoplan Tourliner C13
-    &quot;&quot;, // Neoplan Tourliner C14
-    &quot;&quot;, // Man Lions R07
-    &quot;&quot;, // Man Lions 2023
-    &quot;&quot;, // Mercedes-Benz O-403
-    &quot;&quot;, // Yeni Tourismo 16RHD
-    &quot;&quot;, // Tourismo 16RHD E5 ve E6
-    &quot;&quot;, // Tourismo 17RHD
-    &quot;&quot;, // Travego SE 15SHD
-    &quot;&quot;, // Travego SE 17SHD
-    &quot;&quot;, // Yeni Travego 15SHD
-    &quot;&quot;, // Yeni Travego 16SHD
-    &quot;&quot;, // 2023 Travego 16SHD
-    &quot;&quot;, // Setra S516HD
-    &quot;&quot;, // Setra 517HDH
-    &quot;&quot;, // Setra S531DT
-  
-    // Son eklenen otobüsler
-    &quot;&quot;, // Temsa Maraton 3 VIP 2019-2023
-    &quot;&quot;, // Temsa Safir Plus HD 13
-    &quot;&quot;, // Temsa Safir 2 VIP
-    &quot;&quot;, // Yeni Tourismo 16RHD 2024
-    &quot;&quot;, // 2023 Travego 16SHD
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
 ];
 
 // ETS 2 VERSİYONLARI
 const versions = [
-  	// Otobüsler
-    &quot;v1.53.x&quot;, // Neoplan Cityliner
-    &quot;v1.53.x&quot;, // Neoplan Starliner
-    &quot;v1.53.x&quot;, // Neoplan Tourliner C13
-    &quot;v1.53.x&quot;, // Neoplan Tourliner C14
-    &quot;v1.53.x&quot;, // Man Lions R07
-    &quot;v1.53.x&quot;, // Man Lions 2023
-    &quot;v1.53.x&quot;, // Mercedes-Benz O-403
-    &quot;v1.53.x&quot;, // Yeni Tourismo 16RHD
-    &quot;v1.53.x&quot;, // Tourismo 16RHD E5 ve E6
-    &quot;v1.53.x&quot;, // Tourismo 17RHD
-    &quot;v1.53.x&quot;, // Travego SE 15SHD
-    &quot;v1.53.x&quot;, // Travego SE 17SHD
-    &quot;v1.53.x&quot;, // Yeni Travego 15SHD
-    &quot;v1.53.x&quot;, // Yeni Travego 16SHD
-    &quot;v1.53.x&quot;, // 2023 Travego 16SHD
-    &quot;v1.53.x&quot;, // Setra S516HD
-    &quot;v1.53.x&quot;, // Setra 517HDH
-    &quot;v1.54.x&quot;, // Setra S531DT
-
-    // Haritalar
-    &quot;v1.53.x&quot;, // Paris Map 1:1
-    &quot;v1.53.x&quot;, // Roextended Türkiye Haritası
-    &quot;v1.53.x&quot;, // Grand Utopia 1:1
-    &quot;v1.54.x&quot;, // Taşıyıcı Map
-    &quot;v1.53.x&quot;, // Türkiye YKS Haritası
-  
-    // Son eklenen otobüsler
-    &quot;v1.53.x&quot;, // Temsa Maraton 3 VIP 2019-2023
-    &quot;v1.53.x&quot;, // Temsa Safir Plus HD 13
-    &quot;v1.53.x&quot;, // Temsa Safir 2 VIP
-    &quot;v1.53.x&quot;, // Yeni Tourismo 16RHD 2024
-    &quot;v1.53.x&quot;, // 2023 Travego 16SHD
+    "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x",
+    "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x",
+    "v1.53.x", "v1.54.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.54.x",
+    "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x", "v1.53.x"
 ];
 
 // MOD VERSİYONLARI
 const modVersions = [
-  	// Otobüsler
-    &quot;&quot;, // Neoplan Cityliner
-    &quot;&quot;, // Neoplan Starliner
-    &quot;&quot;, // Neoplan Tourliner C13
-    &quot;&quot;, // Neoplan Tourliner C14
-    &quot;&quot;, // Man Lions R07
-    &quot;v1.2&quot;, // Man Lions 2023
-    &quot;&quot;, // Mercedes-Benz O-403
-    &quot;&quot;, // Yeni Tourismo 16RHD
-    &quot;&quot;, // Tourismo 16RHD E5 ve E6
-    &quot;&quot;, // Tourismo 17RHD
-    &quot;&quot;, // Travego SE 15SHD
-    &quot;&quot;, // Travego SE 17SHD
-    &quot;&quot;, // Yeni Travego 15SHD
-    &quot;&quot;, // Yeni Travego 16SHD
-    &quot;&quot;, // 2023 Travego 16SHD
-    &quot;&quot;, // Setra S516HD
-    &quot;v2&quot;, // Setra 517HDH
-    &quot;v7&quot;, // Setra S531DT
-
-    // Haritalar
-    &quot;v3.4&quot;, // Paris Map 1:1
-    &quot;v4.5&quot;, // Roextended Türkiye Haritası
-    &quot;v1.16.2&quot;, // Grand Utopia 1:1
-    &quot;v11&quot;, // Taşıyıcı Map
-    &quot;v1.4&quot;, // Türkiye YKS Haritası
-  
-    // Son eklenen otobüsler
-    &quot;&quot;, // Temsa Maraton 3 VIP 2019-2023
-    &quot;&quot;, // Temsa Safir Plus HD 13
-    &quot;&quot;, // Temsa Safir 2 VIP
-    &quot;&quot;, // Yeni Tourismo 16RHD 2024
-    &quot;&quot;, // 2023 Travego 16SHD
+    "", "", "", "", "", "v1.2", "", "", "", "", "", "", "", "", "", "v2", "v7",
+    "v3.4", "v4.5", "v1.16.2", "v11", "v1.4", "", "", "", "", ""
 ];
 
 // FİYAT BİLGİLERİ
 const prices = [
-  	// Otobüsler
-    &quot;0,00 &#8378;&quot;, // Neoplan Cityliner
-    &quot;0,00 &#8378;&quot;, // Neoplan Starliner
-    &quot;0,00 &#8378;&quot;, // Neoplan Tourliner C13
-    &quot;0,00 &#8378;&quot;, // Neoplan Tourliner C14
-    &quot;0,00 &#8378;&quot;, // Man Lions R07
-    &quot;0,00 &#8378;&quot;, // Man Lions 2023
-    &quot;50,00 &#8378;&quot;, // Mercedes-Benz O-403
-    &quot;0,00 &#8378;&quot;, // Yeni Tourismo 16RHD
-    &quot;0,00 &#8378;&quot;, // Tourismo 16RHD E5 ve E6
-    &quot;0,00 &#8378;&quot;, // Tourismo 17RHD
-    &quot;0,00 &#8378;&quot;, // Travego SE 15SHD
-    &quot;0,00 &#8378;&quot;, // Travego SE 17SHD
-    &quot;0,00 &#8378;&quot;, // Yeni Travego 15SHD
-    &quot;0,00 &#8378;&quot;, // Yeni Travego 16SHD
-    &quot;0,00 &#8378;&quot;, // 2023 Travego 16SHD
-    &quot;0,00 &#8378;&quot;, // Setra S516HD
-    &quot;0,00 &#8378;&quot;, // Setra 517HDH
-    &quot;80,00 &#8378;&quot;, // Setra S531DT
-
-    // Haritalar
-    &quot;0,00 &#8378;&quot;, // Paris Map 1:1
-    &quot;0,00 &#8378;&quot;, // Roextended Türkiye Haritası
-    &quot;0,00 &#8378;&quot;, // Grand Utopia 1:1
-    &quot;50,00 &#8378;&quot;, // Taşıyıcı Map
-    &quot;0,00 &#8378;&quot;, // Türkiye YKS Haritası
-  
-    // Son eklenen otobüsler
-    &quot;0,00 &#8378;&quot;, // Temsa Maraton 3 VIP 2019-2023
-    &quot;0,00 &#8378;&quot;, // Temsa Safir Plus HD 13
-    &quot;0,00 &#8378;&quot;, // Temsa Safir 2 VIP
-    &quot;0,00 &#8378;&quot;, // Yeni Tourismo 16RHD 2024
-    &quot;0,00 &#8378;&quot;, // 2023 Travego 16SHD
+    "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "50,00 ₺", "0,00 ₺",
+    "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺",
+    "0,00 ₺", "80,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "50,00 ₺", "0,00 ₺", 
+    "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺", "0,00 ₺"
 ];
 
-// Mevcut döngüye fiyat ekleme
-links.forEach((link, index) =&gt; {
+// MODLARI İŞLE
+links.forEach((link, index) => {
     const downloadLink = document.getElementById(`downloadLink${index + 1}`);
     const versionSpan = document.getElementById(`version${index + 1}`);
     const modVersionElement = document.getElementById(`modVer${index + 1}`);
@@ -197,69 +62,71 @@ links.forEach((link, index) =&gt; {
     const statusRefreshElement = document.getElementById(`statusRefresh${index + 1}`);
     const modLinkElement = document.getElementById(`modLink${index + 1}`);
 
-    const imgElement = statusRefreshElement?.closest(&#39;.bus-mods&#39;)?.querySelector(&#39;img&#39;);
-    const busModsContainer = statusRefreshElement?.closest(&#39;.bus-mods&#39;);
+    const imgElement = statusRefreshElement?.closest('.bus-mods')?.querySelector('img');
+    const busModsContainer = statusRefreshElement?.closest('.bus-mods');
 
     if (downloadLink) {
         downloadLink.href = link;
     }
+
     if (versionSpan) {
-        versionSpan.textContent = versions[index] || &quot;Belirtilmemiş&quot;;
+        versionSpan.textContent = versions[index] || "Belirtilmemiş";
     }
+
     if (modVersionElement) {
-        const modVersion = modVersions[index] || &quot;&quot;;
+        const modVersion = modVersions[index] || "";
         modVersionElement.textContent = modVersion;
 
-        if (otherModverElement) {
-            otherModverElement.textContent = modVersion;
-        }
-
-        if (thirdModverElement) {
-            thirdModverElement.textContent = modVersion;
-        }
+        if (otherModverElement) otherModverElement.textContent = modVersion;
+        if (thirdModverElement) thirdModverElement.textContent = modVersion;
     }
+
     if (skinLinkElement) {
         skinLinkElement.href = skinLinks[index];
     }
+
     if (priceElement) {
-        const price = prices[index] || &quot;Fiyat belirtilmemiş&quot;;
+        const price = prices[index] || "Fiyat belirtilmemiş";
         priceElement.innerHTML = price;
 
-        const cleanedPrice = parseFloat(price.replace(&quot;,&quot;, &quot;.&quot;).replace(&quot;&#8378;&quot;, &quot;&quot;));
-        if (price === &quot;0,00 &#8378;&quot;) {
-            priceElement.style.color = &quot;#ff3547&quot;;
-        } else if (cleanedPrice &gt; 0) {
-            priceElement.style.color = &quot;#fff&quot;;
-            priceElement.style.textDecoration = &quot;none&quot;;
+        const cleanedPrice = parseFloat(price.replace(",", ".").replace("₺", ""));
+        if (price === "0,00 ₺") {
+            priceElement.style.color = "#ff3547";
+        } else if (cleanedPrice > 0) {
+            priceElement.style.color = "#fff";
+            priceElement.style.textDecoration = "none";
         }
-        priceElement.style.fontSize = &quot;27px&quot;;
+        priceElement.style.fontSize = "27px";
     }
 
     if (statusRefreshElement) {
-        if (link === &quot;x&quot;) {
-            statusRefreshElement.innerHTML = &quot;<span class='removed' data-uk-icon='icon: close'/><span class='mod-removed'> Mod Kaldırıldı.</span>&quot;;
+        if (link === "x") {
+            statusRefreshElement.innerHTML = "<span class='removed' data-uk-icon='icon: close'></span><span class='mod-removed'> Mod Kaldırıldı.</span>";
+
             if (modLinkElement) {
-    			const anchorTags = modLinkElement.querySelectorAll(&quot;a&quot;);
-    			anchorTags.forEach(anchor =&gt; {
-    			    anchor.removeAttribute(&quot;href&quot;); // href&#39;i kaldır
-    			    anchor.style.color = &quot;#fff !important&quot;; // Rengi beyaz ve önemli yap
-    			    anchor.style.transition = &quot;all 0s ease 0s&quot;; // Geçiş süresini ayarla
-    			});
-			}
+                const anchorTags = modLinkElement.querySelectorAll("a");
+                anchorTags.forEach(anchor => {
+                    anchor.removeAttribute("href");
+                    anchor.style.color = "#fff";
+                    anchor.style.transition = "all 0s ease 0s";
+                });
+            }
+
             if (busModsContainer) {
-                busModsContainer.style.pointerEvents = &quot;none&quot;;
+                busModsContainer.style.pointerEvents = "none";
             }
         } else if (link) {
-            statusRefreshElement.innerHTML = &quot;<span class='updated-check' data-uk-icon='icon: check'/><span class='updated'> Güncellendi.</span>&quot;;
+            statusRefreshElement.innerHTML = "<span class='updated-check' data-uk-icon='icon: check'></span><span class='updated'> Güncellendi.</span>";
+
             if (busModsContainer) {
-                busModsContainer.style.pointerEvents = &quot;auto&quot;;
+                busModsContainer.style.pointerEvents = "auto";
             }
 
             if (imgElement) {
-                imgElement.style.opacity = &quot;1&quot;;  // link === &quot;x&quot; olduğunda opacity 1 yapılır
+                imgElement.style.opacity = "1";
             }
         } else {
-            statusRefreshElement.innerHTML = &quot;<span class='updating-refresh rotate' data-uk-icon='icon: refresh'/><span class='updating'> Mod Güncelleniyor...</span>&quot;;
+            statusRefreshElement.innerHTML = "<span class='updating-refresh rotate' data-uk-icon='icon: refresh'></span><span class='updating'> Mod Güncelleniyor...</span>";
         }
     }
 });
